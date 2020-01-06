607e09118ff4c1decd17ff9bb7aa02dc6f300c74
MadEldar <49936697+MadEldar@users.noreply.github.com>
2020-01-06 16:49:12 +0700

    Lab Session 03

---
+++ Ap/LabSession03/Cart.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabSession03
+{
+    public class Cart
+    {
+        private int _id;
+        private string _name;
+        private int _grandTotal;
+        private Product[] _productList = new Product[0];
+        private string _city;
+        private string _country;
+        
+        public event Notififcation AddingProduct;
+
+        public Cart(int id, string name, string city, string country)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+            Country = country;
+            GrandTotal = CalculateGrandTotal();
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("{0, -3} {1, -20} {2, -10} {3, 1}", Id, Name, City, Country);
+            Console.WriteLine("Products list:");
+            foreach (Product product in ProductList)
+            {
+                product.GetInfo();
+            }
+            Console.WriteLine("Grand total of products: {0, 1}\n", GrandTotal);
+        }
+
+        public void AddToCart(Product product)
+        {
+            ProductList = new List<Product>(ProductList) {product}.ToArray();
+            if (AddingProduct == null)
+            {
+                AddingProduct += DislayMessage;
+            }
+
+            Console.WriteLine(AddingProduct.ToString());
+            
+            AddingProduct("San pham " + product.Name + " da duoc them vao gio hang");
+        }
+
+        public void RemoveFromCart()
+        {
+            Console.WriteLine("Products in cart:");
+            foreach (Product product in ProductList)
+            {
+                product.GetInfo();
+            }
+            Console.WriteLine("Remove product by Id:\n");
+            int idToRemove = Convert.ToInt32(Console.ReadLine());
+            List<Product> products = new List<Product>(ProductList);
+            products.RemoveAll(product => product.Id == idToRemove);
+            ProductList = products.ToArray();
+        }
+
+        public int CalculateGrandTotal()
+        {
+            _grandTotal = 0;
+            foreach (Product product in ProductList)
+            {
+                _grandTotal += product.Price * product.Quantity;
+            }
+            _grandTotal += _grandTotal / (City == "HN" ? 100 : City == "HCM" ? 50 : 20); 
+            
+            return _grandTotal;
+        }
+
+        private void DislayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int GrandTotal
+        {
+            get => CalculateGrandTotal();
+            set => _grandTotal = value;
+        }
+
+        public Product[] ProductList
+        {
+            get => _productList;
+            set => _productList = value;
+        }
+
+        public string City
+        {
+            get => _city;
+            set => _city = value;
+        }
+
+        public string Country
+        {
+            get => _country;
+            set => _country = value;
+        }
+    }
+}
---
+++ Ap/LabSession03/Product.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace LabSession03
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _quantity;
+        private string _image;
+        private string _desc;
+        private string[] _gallery = new string[10];
+
+        private static event Notififcation Event;
+
+        public Product(int id, string name, int price, int quantity, string image, string desc, string[] gallery)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Image = image;
+            Desc = desc;
+            Gallery = gallery;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("{0, -3} {1, -20} {2, -30} {3, -10} {4, -3} {5, -1}",
+                Id, Name, Desc, Price, Quantity, Image);
+        }
+
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (Event == null)
+                {
+                    Event += DisplayMessage;
+                }
+
+                if (value > Quantity)
+                {
+                    Event("So luong san pham tang len " + (value - Quantity) + " san pham, len thanh " + value);
+                }
+                else if (value < Quantity)
+                {
+                    Event("So luong san pham giam di " + (Quantity - value) + " san pham, xuong con " + value);
+                }
+                else
+                {
+                    Event("So luong san pham khong doi tai " + Quantity + " san pham");
+                }
+                
+                _quantity = value;
+            }
+        }
+
+        public string Image
+        {
+            get => _image;
+            set => _image = value;
+        }
+
+        public string Desc
+        {
+            get => _desc;
+            set => _desc = value;
+        }
+
+        public string[] Gallery
+        {
+            get => _gallery;
+            set => _gallery = value;
+        }
+    }
+}
