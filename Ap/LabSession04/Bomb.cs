bde3094743c8a8c337f6330f01a984946a462ddd
MadEldar <49936697+MadEldar@users.noreply.github.com>
2020-01-08 15:03:17 +0700

    Lab Session 04

---
+++ Ap/LabSession04/Bomb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace LabSession04
+{
+    public class Bomb
+    {
+        public Bomb(int timer)
+        {
+            Timer = timer;
+        }
+
+        public int Timer { get; set; }
+
+        public void CountDown()
+        {
+            new Thread(() => TimerThread(this)).Start();
+        }
+
+        private static void TimerThread(Bomb b)
+        {
+            try
+            {
+                for (int i = b.Timer; i > 0; i--)
+                {
+                    Console.WriteLine("{0}:{1}", i/60, i%60);
+                    Thread.Sleep(100);
+                }
+                b.Detonate();
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Detonate()
+        {
+            Console.WriteLine("The bomb goes BOOM.");
+        }
+    }
+}
---
+++ Ap/LabSession04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace LabSession04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bomb b = new Bomb(120);
+            b.CountDown();
+            Bomb b2 = new Bomb(600);
+            b2.CountDown();
+        }
+    }
+}
